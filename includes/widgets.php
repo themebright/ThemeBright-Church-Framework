a100b84ad762eb207860f82913aad8ae89e47f10
Galen Gidman <galen@galengidman.com>
2014-06-25 18:30:52 -0500

    register all widgets

--- includes/widgets.php
+++ includes/widgets.php
@@ -7,8 +7,10 @@ require_once 'classes/widget-sermons.php';
 
 function tbf_register_widgets() {
 
+  register_widget( 'TBF_Widget_Events' );
   register_widget( 'TBF_Widget_Locations' );
   register_widget( 'TBF_Widget_People' );
+  register_widget( 'TBF_Widget_Sermons' );
 
 }
 add_action( 'widgets_init', 'tbf_register_widgets' );
