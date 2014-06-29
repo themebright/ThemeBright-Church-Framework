b6954caa03c300df27dbff8c19ea70e886743be7
Galen Gidman <galen@galengidman.com>
2014-06-29 12:14:44 -0500

    added tbf_theme_url

--- includes/helpers.php
+++ includes/helpers.php
@@ -10,3 +10,18 @@
  * @package ThemeBright_Framework
  * @subpackage Functions
  */
+
+/**
+ * Theme URL.
+ *
+ * Retrieves the URL of the current theme with a trailing slash and no protocol.
+ *
+ * @return string URL of the theme.
+ */
+function tbf_theme_url() {
+
+  $url = tbf_strip_protocol( trailingslashit( get_stylesheet_directory() ) );
+
+  return apply_filters( 'tbf_theme_url', $url );
+
+}
