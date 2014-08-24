67a06f3a735a91fa5e18032b1437df65665df9d8
Galen Gidman <galen@galengidman.com>
2014-08-24 14:56:15 -0500

    added theme version constant

--- framework.php
+++ framework.php
@@ -4,10 +4,18 @@
  */
 
 /**
+ * Get theme data.
+ */
+$theme_data = wp_get_theme();
+$theme_data = is_child_theme() ? wp_get_theme( $theme_data->template ) : $theme_data;
+
+/**
  * Define constants.
  */
-if ( ! defined( 'TBF_VERSION' ) ) define( 'TBF_VERSION', '1.0.0' );
-if ( ! defined( 'TBF_DIR' ) )     define( 'TBF_DIR',     basename( __DIR__ ) );
+if ( ! defined( 'TBF_VERSION' ) )       define( 'TBF_VERSION',       '1.0.0' );
+if ( ! defined( 'TBF_DIR' ) )           define( 'TBF_DIR',           basename( __DIR__ ) );
+
+if ( ! defined( 'TBF_THEME_VERSION' ) ) define( 'TBF_THEME_VERSION', $theme_data->Version );
 
 /**
  * Load includes.
