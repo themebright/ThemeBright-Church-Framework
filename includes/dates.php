ed67375ba80343123b3ea44dd19a13e9bd60db74
Galen Gidman <galen@galengidman.com>
2014-10-01 16:09:00 -0500

    i should really test this stuff

--- includes/dates.php
+++ includes/dates.php
@@ -20,7 +20,11 @@
  * @param string $format PHP date format string to be returned.
  * @return string Formatted date if it exists; contents of $date if it does not.
  */
-function tbf_format_date( $date = null, $format = get_option( 'date_format' ) ) {
+function tbf_format_date( $date = null, $format = null ) {
+
+  if ( ! $format ) {
+    $format = get_option( 'date_format' );
+  }
 
   if ( $date ) {
     $date = date_format( date_create_from_format( 'Y-m-d', $date ), $format );
