8b7d167aaeb2086b613ef63e86a1460f7bf16731
Galen Gidman <galen@galengidman.com>
2014-10-14 13:43:13 -0500

    wups, forgot to rename function

--- includes/helpers.php
+++ includes/helpers.php
@@ -8,7 +8,7 @@
 /**
  * Retrieves the URL of the current template with a trailing slash and no protocol.
  */
-function tbf_template_url() {
+function tbf_get_template_directory_uri() {
 
   $url = trailingslashit( tbf_strip_protocol( get_template_directory_uri() ) );
 
