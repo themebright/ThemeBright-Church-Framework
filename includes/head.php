9c47dea1b8a65d313a9d615504508aa87f3b8959
Galen Gidman <galen@galengidman.com>
2014-10-14 13:53:48 -0500

    cleaned up and renamed tbf_title

--- includes/head.php
+++ includes/head.php
@@ -7,32 +7,21 @@
 
 /**
  * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * Pulled straight from the _s source.
  */
-function tbf_wp_title( $title, $sep ) {
+function tbf_title( $title ) {
 
   if ( is_feed() ) {
     return $title;
   }
 
-  global $page, $paged;
-
-  // add the blog name
   $title .= get_bloginfo( 'name', 'display' );
 
-  // add the blog description for the home/front page
   $site_description = get_bloginfo( 'description', 'display' );
-  if ( $site_description && ( is_home() || is_front_page() ) ) {
-    $title .= " $sep $site_description";
-  }
-
-  // add a page number if necessary
-  if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-    $title .= " $sep " . sprintf( __( 'Page %s', 'tbf' ), max( $paged, $page ) );
+  if ( ! empty( $site_description ) && ( is_home() || is_front_page() ) ) {
+    $title .= " | $site_description";
   }
 
   return $title;
 
 }
-add_filter( 'wp_title', 'tbf_wp_title', 10, 2 );
+add_filter( 'wp_title', 'tbf_title' );
