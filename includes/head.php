67db52f462bed2ec28f426e259a8d63cd0d3d2f1
Galen Gidman <galen@galengidman.com>
2014-10-14 13:59:11 -0500

    nevermind, let's just use unaltered _s

--- includes/head.php
+++ includes/head.php
@@ -7,21 +7,32 @@
 
 /**
  * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * Borrowed from _s.
  */
-function tbf_title( $title ) {
+function tbf_title( $title, $sep ) {
 
   if ( is_feed() ) {
     return $title;
   }
 
+  global $page, $paged;
+
+  // Add the blog name
   $title .= get_bloginfo( 'name', 'display' );
 
+  // Add the blog description for the home/front page.
   $site_description = get_bloginfo( 'description', 'display' );
-  if ( ! empty( $site_description ) && ( is_home() || is_front_page() ) ) {
-    $title .= " | $site_description";
+  if ( $site_description && ( is_home() || is_front_page() ) ) {
+    $title .= " $sep $site_description";
+  }
+
+  // Add a page number if necessary:
+  if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+    $title .= " $sep " . sprintf( __( 'Page %s', '_s' ), max( $paged, $page ) );
   }
 
   return $title;
 
 }
-add_filter( 'wp_title', 'tbf_title' );
+add_filter( 'wp_title', 'tbf_title', 10, 2 );
