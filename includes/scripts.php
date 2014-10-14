243c119fa1e13a236485e920c8748287dc22e254
Galen Gidman <galen@galengidman.com>
2014-10-14 13:42:49 -0500

    tbf_template_url → tbf_get_template_directory_uri

--- includes/scripts.php
+++ includes/scripts.php
@@ -12,10 +12,10 @@ function tbf_scripts() {
 
   // Register scripts for use later
   wp_register_script( 'tbf-maps-api', '//maps.googleapis.com/maps/api/js?v=3.exp&sensor=false', 'false', TBF_VERSION );
-  wp_register_script( 'tbf-maps', tbf_template_url() . TBF_DIR . '/assets/js/maps.js', array( 'jquery', 'tbf-maps-api' ), TBF_VERSION );
+  wp_register_script( 'tbf-maps', tbf_get_template_directory_uri() . TBF_DIR . '/assets/js/maps.js', array( 'jquery', 'tbf-maps-api' ), TBF_VERSION );
 
   // Immediately enqueue these scripts
-  // wp_enqueue_script( 'tbf', tbf_template_url() . TBF_DIR . '/assets/js/tbf.js', array( 'jquery' ), TBF_VERSION );
+  // wp_enqueue_script( 'tbf', tbf_get_template_directory_uri() . TBF_DIR . '/assets/js/tbf.js', array( 'jquery' ), TBF_VERSION );
 
 }
 add_action( 'wp_enqueue_scripts', 'tbf_scripts' );
