236ea3f0b05082a3c7c42d41633c940bea366ea3
Galen Gidman <galen@galengidman.com>
2015-09-02 22:13:50 -0500

    alignment

--- includes/widgets.php
+++ includes/widgets.php
@@ -20,10 +20,10 @@ function tbf_register_widgets() {
 	$theme_support = $theme_support[0]['widgets'];
 
 	if ( $theme_support ) {
-		if ( array_key_exists( 'events', $theme_support ) )    register_widget( 'TBF_Widget_Events' );
+		if ( array_key_exists( 'events',    $theme_support ) ) register_widget( 'TBF_Widget_Events' );
 		if ( array_key_exists( 'locations', $theme_support ) ) register_widget( 'TBF_Widget_Locations' );
-		if ( array_key_exists( 'people', $theme_support ) )    register_widget( 'TBF_Widget_People' );
-		if ( array_key_exists( 'sermons', $theme_support ) )   register_widget( 'TBF_Widget_Sermons' );
+		if ( array_key_exists( 'people',    $theme_support ) ) register_widget( 'TBF_Widget_People' );
+		if ( array_key_exists( 'sermons',   $theme_support ) ) register_widget( 'TBF_Widget_Sermons' );
 	}
 
 }
