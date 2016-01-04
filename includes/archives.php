de693ac58f7febd249d450d720accf13c938838d
Galen Gidman <galen@galengidman.com>
2016-01-04 15:44:40 -0600

    on second thought, just disable CTC archives altogether if theme supports archive redirection

--- includes/archives.php
+++ includes/archives.php
@@ -41,29 +41,29 @@ function tbf_get_archive_replacement_pages( $post_type = null ) {
 /**
  * Update archive slugs to new URLs.
  */
-function tbf_update_archive_slugs() {
+function tbf_disable_archives() {
 
 	$theme_support = get_theme_support( 'tbf' );
 
 	if ( ! empty( $theme_support ) && in_array( 'archive_redirection', $theme_support[0] ) ) {
 
 		add_filter( 'ctc_post_type_event_args', function( $args ) {
-			$args['rewrite']['slug'] = 'ctc-events';
+			$args['has_archive'] = false;
 			return $args;
 		} );
 
 		add_filter( 'ctc_post_type_location_args', function( $args ) {
-			$args['rewrite']['slug'] = 'ctc-locations';
+			$args['has_archive'] = false;
 			return $args;
 		} );
 
 		add_filter( 'ctc_post_type_person_args', function( $args ) {
-			$args['rewrite']['slug'] = 'ctc-people';
+			$args['has_archive'] = false;
 			return $args;
 		} );
 
 		add_filter( 'ctc_post_type_sermon_args', function( $args ) {
-			$args['rewrite']['slug'] = 'ctc-sermons';
+			$args['has_archive'] = false;
 			return $args;
 		} );
 
@@ -72,26 +72,4 @@ function tbf_update_archive_slugs() {
 	return false;
 
 }
-add_action( 'after_setup_theme', 'tbf_update_archive_slugs', 999 );
-
-/**
- * Redirect archives to oldest page with appropriate template if theme support allows.
- */
-function tbf_redirect_archives_to_pages( $query ) {
-
-	$theme_support = get_theme_support( 'tbf' );
-	$post_types    = array( 'ctc_event', 'ctc_location', 'ctc_person', 'ctc_sermon' );
-
-	if ( ! is_admin() && $query->is_main_query() && ! empty( $theme_support ) && in_array( 'archive_redirection', $theme_support[0] ) && is_post_type_archive( $post_types ) ) {
-		$pages = tbf_get_archive_replacement_pages( $query->query['post_type'] );
-
-		if ( count( $pages ) > 0 ) {
-			wp_redirect( esc_url( get_permalink( $pages[0]->ID ) ) );
-			exit;
-		}
-	}
-
-	return false;
-
-}
-add_action( 'pre_get_posts', 'tbf_redirect_archives_to_pages' );
+add_action( 'after_setup_theme', 'tbf_disable_archives', 999 );
