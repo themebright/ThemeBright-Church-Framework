8f0b26c4ea7220b07a713ca66b98c52c6b0975df
Galen Gidman <galen@galengidman.com>
2016-01-05 12:13:12 -0600

    only register widgets if CTC is active

--- includes/widgets.php
+++ includes/widgets.php
@@ -19,7 +19,7 @@ function tbf_register_widgets() {
 	$theme_support = get_theme_support( 'tbf' );
 	$theme_support = $theme_support[0]['widgets'];
 
-	if ( $theme_support ) {
+	if ( $theme_support && defined( 'CTC_VERSION' ) ) {
 		if ( array_key_exists( 'events',    $theme_support ) ) register_widget( 'TBF_Widget_Events' );
 		if ( array_key_exists( 'locations', $theme_support ) ) register_widget( 'TBF_Widget_Locations' );
 		if ( array_key_exists( 'people',    $theme_support ) ) register_widget( 'TBF_Widget_People' );
