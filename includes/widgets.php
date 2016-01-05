f409f7341a0f456744b514704df79cd08d097d3c
Galen Gidman <galen@galengidman.com>
2016-01-05 16:11:17 -0600

    only load widget classes if needed

--- includes/widgets.php
+++ includes/widgets.php
@@ -4,15 +4,7 @@
  */
 
 /**
- * Loads widget classes.
- */
-require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-events.php';
-require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-locations.php';
-require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-people.php';
-require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-sermons.php';
-
-/**
- * Registers widgets classes.
+ * Loads and registers widgets classes.
  */
 function tbf_register_widgets() {
 
@@ -20,6 +12,11 @@ function tbf_register_widgets() {
 	$theme_support = $theme_support[0]['widgets'];
 
 	if ( $theme_support && defined( 'CTC_VERSION' ) ) {
+		require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-events.php';
+		require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-locations.php';
+		require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-people.php';
+		require_once get_template_directory() . '/' . TBF_DIR . '/includes/classes/widget-sermons.php';
+
 		if ( array_key_exists( 'events',    $theme_support ) ) register_widget( 'TBF_Widget_Events' );
 		if ( array_key_exists( 'locations', $theme_support ) ) register_widget( 'TBF_Widget_Locations' );
 		if ( array_key_exists( 'people',    $theme_support ) ) register_widget( 'TBF_Widget_People' );
