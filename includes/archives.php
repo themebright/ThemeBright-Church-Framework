4948f23b041eaa5992aafb758c14e986385da260
Galen Gidman <galen@galengidman.com>
2016-05-02 13:07:43 -0500

    disable archive redirection on search results

--- includes/archives.php
+++ includes/archives.php
@@ -45,7 +45,7 @@ function tbcf_redirect_archives() {
 
 	$theme_support = get_theme_support( 'tbcf' );
 
-	if ( ! empty( $theme_support ) && in_array( 'archive_redirection', $theme_support[0] ) ) {
+	if ( ! empty( $theme_support ) && in_array( 'archive_redirection', $theme_support[0] ) && ! is_search() ) {
 		if ( is_post_type_archive( array( 'ctc_event', 'ctc_location', 'ctc_person', 'ctc_sermon' ) ) ) {
 			$page = tbcf_get_archive_replacement_pages( get_post_type() );
 
